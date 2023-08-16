612ab3f5ea7fd255025cf77ef116c873c60c56af
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-08-16 22:06:03 +0500

    fixing

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,11 @@
-# frozen_string_literal: true
+require_relative "boot"
 
-require_relative 'boot'
+require "rails/all"
 
-require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module HelloRailsBackEnd
+module HelloBackend
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
     # ... Other configuration settings ...
@@ -21,15 +16,15 @@ module HelloRailsBackEnd
         origins 'http://localhost:5173' # Update with your React frontend's URL
 
         resource '/api/v1/*',
-                 headers: :any,
-                 methods: %i[get post put patch delete options head],
-                 credentials: true
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
       end
     end
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    # Skip views, helpers, and assets when generating a new resource.
     config.api_only = true
   end
 end
